404230cf438b67ae9f7156a9cee2d5273bf0ad4e
gabrdean <gabealexd@gmail.com>
2018-05-21 16:53:23 -0400

    Finally getting some-goddamn-where. Made a textbox for user title and linked it to Main activity code. Made list view for tasks and their dates which is also linked. Thinking about switching to custom list view.  Btw scrollview is not necessary when scrolling vertically, list view does it automatically.

--- app/src/main/java/com/haverford/benjstal/selfdevapp/MainTasks.java
+++ app/src/main/java/com/haverford/benjstal/selfdevapp/MainTasks.java
@@ -8,6 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainTasks extends AppCompatActivity {
 
@@ -15,6 +21,18 @@ public class MainTasks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_tasks);
+
+        String[] dates = {"May 21", "May22", "May 23"};
+        ListAdapter testAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dates);
+        ListView testListView = (ListView) findViewById(R.id.testListView);
+        testListView.setAdapter(testAdapter);
+
+        final TextView wordsmithTextView = (TextView) findViewById(R.id.wordsmithTextView);
+
+        String[] title_type = {"Vagabond","Nomad","Charmer","Champion"};
+        String Title = "Title:" + title_type[3];
+        wordsmithTextView.setText(Title);
+
 
     }
 

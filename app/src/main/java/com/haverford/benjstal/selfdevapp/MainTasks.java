567ab0ffe0fa60076638c381ed3fb02162cc9923
benjstal <bsgtths@gmail.com>
2018-05-22 14:54:37 -0400

    changed the words next to ProfileiImageButton to wrap_content for width(made it one line in the app) changed id of imageButton to "profilebutton" declared "testAdapter" and "dates" at the top of the file to fix errors wrote code so tha when imageButton is clicked it goes to a new blank activity(this activity will be the screen with the personal profile info)

--- app/src/main/java/com/haverford/benjstal/selfdevapp/MainTasks.java
+++ app/src/main/java/com/haverford/benjstal/selfdevapp/MainTasks.java
@@ -1,28 +1,35 @@
 package com.haverford.benjstal.selfdevapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.List;
-
 public class MainTasks extends AppCompatActivity {
+
+    ImageButton profilebutton;
+    String[] dates = {"May 21", "May22", "May 23"};
+    ArrayAdapter testAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_tasks);
 
-        String[] dates = {"May 21", "May22", "May 23"};
+        profilebutton = (ImageButton) findViewById(R.id.profilebutton);
+        profilebutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                openProfileScreen();
+            }
+        });
+
         ListAdapter testAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dates);
         ListView testListView = (ListView) findViewById(R.id.testListView);
         testListView.setAdapter(testAdapter);
@@ -34,6 +41,11 @@ public class MainTasks extends AppCompatActivity {
         wordsmithTextView.setText(Title);
 
 
+    }
+
+    public void openProfileScreen() {
+        Intent openProfileActivity = new Intent(this, ProfileScreen.class);
+        startActivity(openProfileActivity);
     }
 
 }

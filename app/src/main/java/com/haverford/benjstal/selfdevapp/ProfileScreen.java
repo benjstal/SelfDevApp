a8b6000836c8f88eb8cffebfbc1e8aabab57bf4d
benjstal <bsgtths@gmail.com>
2018-05-23 11:38:42 -0400

    changed the words next to ProfileiImageButton to wrap_content for width(made it one line in the app) changed id of imageButton to "profilebutton" declared "testAdapter" and "dates" at the top of the file to fix errors wrote code so tha when imageButton is clicked it goes to a new blank activity(this activity will be the screen with the personal profile info)

---
+++ app/src/main/java/com/haverford/benjstal/selfdevapp/ProfileScreen.java
@@ -0,0 +1,13 @@
+package com.haverford.benjstal.selfdevapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_screen);
+    }
+}
---
+++ app/src/main/java/com/haverford/benjstal/selfdevapp/Task.java
@@ -0,0 +1,14 @@
+package com.haverford.benjstal.selfdevapp;
+
+public class Task {
+    String title;
+    int xpboost;
+    char category;
+    String description;
+
+    public Task("tit", ) {
+        this.title = tit
+    }
+
+
+}
